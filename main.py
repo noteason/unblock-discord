db46df59fdf1b79d0d1ba65a64c3973ebf6d6389
noteason <74929228+noteason@users.noreply.github.com>
2022-02-16 13:39:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-import os
 import bs4
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
 import asyncio
-import time
-
-
 ua = UserAgent()
-
 options = webdriver.ChromeOptions()
 options.add_argument('--no-sandbox')
 options.add_argument("user-agent="+ua.random)
 driver = webdriver.Chrome(options=options)
-
 driver.get(f"https://discord.gg/")
-# u will be asked to login to discord on this page, just use it like normally
